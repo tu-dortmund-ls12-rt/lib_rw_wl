7a5c5ac310a3398099593c2a078b157e01a697b7
Christian Hakert <christian.hakert@tu-dortmund.de>
2020-01-09 07:24:22 +0100

    Implemented page permission functionality, to make observed pages generate IRQs on access

--- writemonitor.c
+++ writemonitor.c
@@ -1,3 +1,4 @@
+#include <arm/arm64/mmu.h>
 #include <arm/arm64/pmc_64.h>
 #include <gic/gic-v2.h>
 #include <stdint.h>
@@ -12,6 +13,7 @@ unsigned long
 // This defines the offset where the monitoring starts to listen, it is usually
 // the first page of the application
 unsigned long uk_so_wl_monitor_offset = 0;
+unsigned long uk_so_wl_number_pages = 0;
 
 int uk_so_wl_writemonitor_handle_overflow(void* arg) {
     arm64_pmc_write_event_counter(
@@ -47,6 +49,16 @@ void uk_so_wl_writemonitor_set_monitor_offset(unsigned long offset) {
     uk_so_wl_monitor_offset = offset;
 }
 
+void uk_so_wl_writemonitor_set_number_pages(unsigned long number_pages) {
+    if (number_pages > CONFIG_SOFTONLYWEARLEVELINGLIB_MONITOR_CAPACITY) {
+        UK_CRASH(
+            "Too many pages to monitor are requested. Requested %u, bot only "
+            "%u are available. Please change the configuration accordingly.\n",
+            number_pages, CONFIG_SOFTONLYWEARLEVELINGLIB_MONITOR_CAPACITY);
+    }
+    uk_so_wl_number_pages = number_pages;
+}
+
 void uk_so_wl_writemonitor_terminate() {
     gic_disable_irq(320);
     // Set observed pages logic
@@ -56,14 +68,18 @@ void uk_so_wl_writemonitor_terminate() {
 
 void uk_so_wl_writemonitor_plot_results() {
     // Print out write counts for all approximated pages
-    for (unsigned long i = 0;
-         i < CONFIG_SOFTONLYWEARLEVELINGLIB_MONITOR_CAPACITY; i++) {
+    for (unsigned long i = 0; i < uk_so_wl_number_pages; i++) {
         // Print out the address and the approximation
         printf("%x %u\n", (uk_so_wl_monitor_offset + i * 0x1000),
                uk_so_wl_write_count[i]);
     }
 }
 
-void uk_so_wl_writemonitor_set_page_mode(int generate_interrupts){
-    
+void uk_so_wl_writemonitor_set_page_mode(int generate_interrupts) {
+    for (unsigned long i = 0; i < uk_so_wl_number_pages; i++) {
+        plat_mmu_set_access_permissions(
+            uk_so_wl_monitor_offset + i * 0x1000,
+            generate_interrupts ? PLAT_MMU_PERMISSION_R_FROM_OS_USER
+                                : PLAT_MMU_PERMISSION_RW_FROM_OS_USER);
+    }
 }
