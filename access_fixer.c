a29da6104d925c1d807a3d93ebf5c6e03ade322e
Christian Hakert <christian.hakert@tu-dortmund.de>
2020-03-20 16:28:37 +0100

    volatile structures

--- access_fixer.c
+++ access_fixer.c
@@ -1,5 +1,5 @@
-#include <uk/config.h>
 #include <stdio.h>
+#include <uk/config.h>
 
 #ifdef CONFIG_SOFTONLYWEARLEVELINGLIB_DO_TEXT_PAGE_CONSITENCY
 
@@ -72,6 +72,48 @@ void uk_upper_level_data_abort_handler(unsigned long *register_stack) {
                 ((elr - CONFIG_SPARE_VM_BASE) %
                  (uk_so_wl_text_spare_vm_size * 0x1000)) +
                 uk_app_base + 0x1000;
+            volatile struct armv8_ldr_instr *instr =
+                (volatile struct armv8_ldr_instr *)(plain_instr);
+            // LDR with immediare and unsigned offset
+            if (instr->opcode2 == 0b1111100101) {
+                // printf("LDR found\n");
+                unsigned long reg = instr->rn;
+                unsigned long fvalue = register_stack[reg];
+                if (fvalue >= CONFIG_SPARE_VM_BASE) {
+                    unsigned long offset =
+                        (fvalue - CONFIG_SPARE_VM_BASE) %
+                        (uk_so_wl_text_spare_vm_size * 0x1000);
+                    fvalue = uk_so_wl_text_spare_vm_begin + offset;
+                    register_stack[reg] = fvalue;
+                } else {
+                    printf(
+                        "ERROR: Faulting register does not conatin spare "
+                        "value (0x%lx at X%d)\n",
+                        fvalue, reg);
+                    printf(
+                        "PFAR is %d, while text ends at %d and got ends at "
+                        "%d\n",
+                        p_far, 2 * uk_app_text_size,
+                        2 * uk_app_text_size + uk_app_got_size);
+                    while (1)
+                        ;
+                }
+            } else {
+                unsigned int *instr_arr = ((unsigned int *)plain_instr);
+                printf(
+                    "ERROR: Invalid cannot parse current instruction type at "
+                    "0x%lx (0x%x), plain instr is 0x%lx, far is 0x%lx, instr "
+                    "at elr is 0x0\n",
+                    elr, instr_arr[0], plain_instr, far);
+                for (unsigned long i = 0; i < 128; i++) {
+                    printf("0x%x\n", instr_arr[i]);
+                }
+                while (1)
+                    ;
+            }
+        } else {
+            printf("Abort did not happen in the app at (0x%lx)\n", elr);
+            unsigned long plain_instr = elr;
             struct armv8_ldr_instr *instr =
                 (struct armv8_ldr_instr *)(plain_instr);
             // LDR with immediare and unsigned offset
@@ -92,11 +134,12 @@ void uk_upper_level_data_abort_handler(unsigned long *register_stack) {
                     while (1)
                         ;
                 }
+            } else {
+                printf(
+                    "ERROR: Invalid cannot parse current instruction type\n");
+                while (1)
+                    ;
             }
-        } else {
-            printf("ERROR: Abort did not happen in the app, cannot handle\n");
-            while (1)
-                ;
         }
     } else {
         printf("ERROR: Invalid abort on non PLT/GOT address\n");
